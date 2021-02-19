58b03a0f2c66bb07d6955495a0838d9c22e7fd35
Erik Huang <erikhuangdrums@gmail.com>
2021-02-19 07:46:08 -0800

    change home page greeting to reflect time of day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,17 @@ class ApplicationController < ActionController::Base
         DateTime.now.strftime('%B %e, %Y')
     end
 
+    def greeting(name)
+        time = DateTime.now.hour
+        if time >= 0 && time < 12
+            "Good morning, #{name}"
+        elsif time >= 12 && time < 18
+            "Good afternoon, #{name}"
+        else
+            "Good evening, #{name}"
+        end
+    end
+
     def current_user
         User.find_by(id: session[:user_id])
     end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-    helper_method :current_user, :logged_in?, :date_today
+    helper_method :current_user, :logged_in?, :date_today, :greeting
     before_action :require_login
 
     def home
